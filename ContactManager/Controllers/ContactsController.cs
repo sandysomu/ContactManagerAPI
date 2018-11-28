6bb6f9a2745bd850b0ef5f680de530d1a4148a15
Sandeep Singh <sandeep.singh@williamhill.com.au>
2018-11-28 15:07:05 +1100

    Added Swagger

---
+++ ContactManager/Controllers/ContactsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+
+
+        [HttpGet("GetContactInfo{name}")]
+        public ActionResult<string> Get(string name)
+        {
+            return Ok(name);
+        }
+
+
+    }
+}
