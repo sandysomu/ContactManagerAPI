caaf53af9782ff3c01658ad780351043b674ef13
Sandeep Singh <sandeep.singh@williamhill.com.au>
2019-02-21 16:27:03 +1100

    Added API test project

---
+++ ContentManagerAPI.Test/UnitTest1.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ContentManagerAPI.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task Test1()
+        {
+            string url = "http://localhost:8080/Contacts/GetContactInfo/saanu";
+            //var client = new HttpClient();
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri(url)
+            };
+
+            //  client.BaseAddress = new Uri(url);
+
+            HttpResponseMessage response = await client.GetAsync(client.BaseAddress);
+
+            var myRootObject = await response.Content.ReadAsAsync<RootObject>();
+        }
+    }
+
+
+    public class RootObject
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int PhoneNumber { get; set; }
+    }
+
+}
